c15fefbbcf27b99e56f207d6d0d33d744cea8c90
Ash Sendell-Price <36015770+ASendellPrice@users.noreply.github.com>
2023-05-31 10:30:05 +0100

    Update pathsafeQC_assembler_set2.sh

--- scripts/pathsafeQC_assembler_set2.sh
+++ scripts/pathsafeQC_assembler_set2.sh
@@ -27,12 +27,3 @@ masurca -i ../EToKi_prepare_L1_R1.fastq.gz,../EToKi_prepare_L1_R2.fastq.gz
 mv CA/primary.genome.scf.fasta ../../assembly_fastas/masurca.fasta
 cd $WORKING_DIR
 
-# Build assembly using abyss
-# mkdir abyss 
-# cd abyss
-# abyss-pe name=abyss k=96 B=2G in='../../input_files/raw_forward.fastq.gz ../input_files/raw_reverse.fastq.gz'
-# mv abyss-scaffolds.fa ../../assembly_fastas/abyss.fasta
-# cd $WORKING_DIRd
-
-
-ABYSS -k 96 -o abyss.fa ../input_files/raw_forward.fastq.gz ../input_files/raw_reverse.fastq.gz
